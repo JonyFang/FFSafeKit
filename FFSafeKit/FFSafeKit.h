bd43e97db0dfc49db2d5b3a20ed84a93e61d0ff6
JonyFang <jony.chunfang@gmail.com>
2019-07-04 15:23:46 +0800

    init project
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

---
+++ FFSafeKit/FFSafeKit.h
@@ -0,0 +1,19 @@
+//
+//  FFSafeKit.h
+//  FFSafeKit
+//
+//  Created by JonyFang on 2019/7/4.
+//  Copyright © 2019 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFSafeKit.
+FOUNDATION_EXPORT double FFSafeKitVersionNumber;
+
+//! Project version string for FFSafeKit.
+FOUNDATION_EXPORT const unsigned char FFSafeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFSafeKit/PublicHeader.h>
+
+
---
+++ FFSafeKitTests/FFSafeKitTests.m
@@ -0,0 +1,37 @@
+//
+//  FFSafeKitTests.m
+//  FFSafeKitTests
+//
+//  Created by JonyFang on 2019/7/4.
+//  Copyright © 2019 JonyFang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FFSafeKitTests : XCTestCase
+
+@end
+
+@implementation FFSafeKitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
