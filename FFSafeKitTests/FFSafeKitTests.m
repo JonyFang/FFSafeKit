dee8ef841a7fd485d26bc7b8837df539b2d2a8d9
JonyFang <jony.chunfang@gmail.com>
2019-07-05 18:17:05 +0800

    add unit test
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

--- FFSafeKitTests/FFSafeKitTests.m
+++ FFSafeKitTests/FFSafeKitTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "FFSafeKit.h"
 
 @interface FFSafeKitTests : XCTestCase
 
@@ -16,10 +17,12 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    [super setUp];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
 }
 
 - (void)testExample {
@@ -32,6 +35,32 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+//NSArray safety checker
+- (void)testSafeArray {
+    NSArray *array = @[@"0", @"1", @"2", @"3"];
+    
+    //Index is equal to `array.count`
+    NSString *equalIndex = array[4];
+    XCTAssertEqual(equalIndex, nil, @"Index is equal to the value returned by `array.count`");
+    
+    //Index is greater than `array.count`
+    NSString *greaterIndex = array[5];
+    XCTAssertEqual(greaterIndex, nil, @"Index is greater than the value returned by `array.count`");
+}
+
+//NSMutableArray safety checker
+- (void)testSafeMutableArray {
+    NSMutableArray *mArray = [[NSMutableArray alloc] initWithArray:@[@"0", @"1", @"2", @"3"]];
+    
+    //Index is equal to `mArray.count`
+    NSString *equalIndex = mArray[4];
+    XCTAssertEqual(equalIndex, nil, @"Index is equal to the value returned by `mArray.count`");
+    
+    //Index is greater than `mArray.count`
+    NSString *greaterIndex = mArray[5];
+    XCTAssertEqual(greaterIndex, nil, @"Index is greater than the value returned by `mArray.count`");
 }
 
 @end
