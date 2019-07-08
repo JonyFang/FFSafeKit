e85f4664c19d9e841745f98072538466a114eb4e
JonyFang <jony.chunfang@gmail.com>
2019-07-08 11:43:51 +0800

    optimize dictionary safty
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

--- FFSafeKit/Classes/NSDictionary+FFSafeKit.m
+++ FFSafeKit/Classes/NSDictionary+FFSafeKit.m
@@ -7,42 +7,77 @@
 //
 
 #import "NSDictionary+FFSafeKit.h"
-#import "NSObject+FFSafeImp.h"
+#import "NSObject+FFSafeSwizzling.h"
 #import <objc/runtime.h>
 
 @implementation NSDictionary (FFSafeKit)
 
 #pragma mark - Load Method
 + (void)load{
-    NSString *systemMethod = @"initWithObjects:forKeys:count:";
-    NSString *systemClassStr = @"__NSPlaceholderDictionary";
-    NSString *safeMethodStr = @"initWithObjects_safe:forKeys:count:";
-    NSString *targetClassStr = @"NSDictionary";
-    
-    [self ff_swizzlingMethod:systemMethod systemClassString:systemClassStr toSafeMethodString:safeMethodStr targetClassString:targetClassStr];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self ff_exchangeInstanceMethodOfClass:NSClassFromString(@"__NSPlaceholderDictionary") originalSelector:@selector(initWithObjects:forKeys:count:) newSelector:@selector(ff_initWithObjects:forKeys:count:)];
+        [self ff_exchangeInstanceMethodOfClass:NSClassFromString(@"__NSPlaceholderDictionary") originalSelector:@selector(initWithObjects:forKeys:) newSelector:@selector(ff_initWithObjects:forKeys:)];
+    });
 }
 
 #pragma mark - Implement Methods
+/**
+ Initializes a newly allocated dictionary with key-value pairs constructed from the provided arrays of keys and objects.
+ It's similar to `initWithObjects:forKeys:`, but it never throw exception.
+ 
+ @param objects An array containing the values for the new dictionary.
+ @param keys An array containing the keys for the new dictionary.
+ */
+- (instancetype)ff_initWithObjects:(NSArray *)objects forKeys:(NSArray<id<NSCopying>> *)keys {
+    NSUInteger count = MIN(objects.count, keys.count);
+    NSMutableArray *newKeys = [NSMutableArray array];
+    NSMutableArray *newObjects = [NSMutableArray array];
+    
+    for (NSUInteger i = 0; i < count; i++) {
+        id key = keys[i];
+        id object = objects[i];
+        if (!key) {
+            continue;
+        }
+        if (!object) {
+            object = [NSNull null];
+        }
+        [newKeys addObject:key];
+        [newObjects addObject:object];
+    }
+    
+    return [self ff_initWithObjects:newObjects forKeys:newKeys];
+}
+
 /**
  Initializes a newly allocated dictionary with the specified number of key-value pairs constructed from the provided C arrays of keys and objects.
  It's similar to `initWithObjects:forKeys:count:`, but it never throw exception.
  
  @param objects A C array of values for the new dictionary.
  @param keys A C array of keys for the new dictionary. Each key is copied (using copyWithZone:; keys must conform to the NSCopying protocol), and the copy is added to the new dictionary.
- @param count The number of elements to use from the keys and objects arrays. count must not exceed the number of elements in objects or keys.
+ @param cnt The number of elements to use from the keys and objects arrays. count must not exceed the number of elements in objects or keys.
  */
-- (instancetype)initWithObjects_safe:(id *)objects forKeys:(id<NSCopying> *)keys count:(NSUInteger)count {
-    NSUInteger rightCount = 0;
-    for (NSUInteger i = 0; i < count; i++) {
-        if (!(keys[i] && objects[i])) {
-            break;
-        } else {
-            rightCount++;
-        }
-    }
-    self = [self initWithObjects_safe:objects forKeys:keys count:rightCount];
+- (instancetype)ff_initWithObjects:(id  _Nonnull const [])objects forKeys:(id<NSCopying>  _Nonnull const [])keys count:(NSUInteger)cnt {
+    id newKeys[cnt];
+    id newObjects[cnt];
+    NSUInteger index = 0;
     
-    return self;
+    for (NSUInteger i = 0; i < cnt; i++) {
+        id key = keys[i];
+        id object = objects[i];
+        if (!key) {
+            continue;
+        }
+        if (!object) {
+            object = [NSNull null];
+        }
+        newKeys[index] = key;
+        newObjects[index] = object;
+        index++;
+    }
+    
+    return [self ff_initWithObjects:newObjects forKeys:newKeys count:index];
 }
 
 @end
